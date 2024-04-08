33f5861b875d5180c87e661d159392c493c7a63e
rvaldez20 <rvaldez20@gmail.com>
2024-04-08 00:25:45 -0600

    [ADD] start init styles: header and container of form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl">Expense Control</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Expense Control
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <p>Formulario Aqui</p>
+      </div>
     </>
   )
 }
