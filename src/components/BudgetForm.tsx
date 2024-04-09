bd338b413c903402f5e8014def14f3250d446f62
rvaldez20 <rvaldez20@gmail.com>
2024-04-08 19:28:23 -0600

    [ADD] validate the form with valid budget: greater than 0 and the input has value positive

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,44 @@
-
+import { useState, ChangeEvent, useMemo } from 'react'
 
 
 export default function BudgetForm() {
-  return (
-    <div>BudgetForm</div>
-  )
+   const [budget, setBudget] = useState(0)
+
+   const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+      // console.log(e.target.valueAsNumber)
+      setBudget(e.target.valueAsNumber)
+   }
+
+   const isValid = useMemo(() => {
+      return isNaN(budget) || budget <= 0
+   }, [budget])
+
+   return (
+      <form
+         className="space-y-5"
+      >
+         <div className="flex flex-col space-y-5">
+            <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+               Definir Presupuesto
+            </label>
+            <input
+               id="budgetID"
+               type="number"
+               className="w-full bg-white border border-gray-300 p-2"
+               placeholder="Define tu presupuesto"
+               name="budget"
+               value={budget}
+               onChange={handleChange}
+            />
+
+         </div>
+
+         <input 
+            type="submit"
+            value="Definir Presupuesto"
+            className="bg-blue-600 hover:bg-blue-700 text-white w-full py-2 cursor-pointer font-black uppercase disabled:opacity-25"
+            disabled={isValid}
+         />
+      </form>
+   )
 }
