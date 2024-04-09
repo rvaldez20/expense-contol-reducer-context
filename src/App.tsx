fe2f7888d6f2be4fea6ef62672c01d326579ed9c
rvaldez20 <rvaldez20@gmail.com>
2024-04-08 22:30:01 -0600

    [ADD] using useBodget check the state if haave valid budget show component BudgetTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useMemo } from 'react'
 import BudgetForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from './components/BudgetTracker';
 
 function App() {
+
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
@@ -10,7 +18,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker />  : <BudgetForm />}
       </div>
     </>
   )
---
+++ src/components/BudgetTracker.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function BudgetTracker() {
+  return (
+    <div>
+      BudgetTracker
+    </div>
+  )
+}
