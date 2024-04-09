11f412f96583ca13abefc4f7810f189edb0909e7
rvaldez20 <rvaldez20@gmail.com>
2024-04-08 21:40:35 -0600

    [ADD] import and use the custom hook useBudget

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react"
 import BudgetForm from "./components/BudgetForm"
-import { BudgetContext } from "./context/BudgetContext"
+import { useBudget } from "./hooks/useBudget"
 
 function App() {
 
-  const context = useContext(BudgetContext)
-  // console.log(context)
+  const { state, dispatch } = useBudget()  
+  // console.log(state)
 
   return (
     <>
