d455ffb34b299179c1132505a1abbdd17a93a97f
rvaldez20 <rvaldez20@gmail.com>
2024-04-09 18:05:01 -0600

    [ADD] render the modal and validate for show button to add expnse when set budget

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from './components/BudgetTracker';
+import ExpenseModal from './components/ExpenseModal';
 
 function App() {
 
@@ -20,6 +21,14 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker />  : <BudgetForm />}
       </div>
+
+      {isValidBudget && (
+        <main className='max-w-3xl mx-auto py10'>
+
+          
+          <ExpenseModal />
+        </main>
+      )}
     </>
   )
 }
