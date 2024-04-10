84d2f558a8ca7305815ce34c66b582e8c55ee177
rvaldez20 <rvaldez20@gmail.com>
2024-04-09 23:00:50 -0600

    [ADD] use type Category for the array of categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,7 @@
+import { Category } from "../types"
 
-export const categories = [
+
+export const categories: Category[] = [
    { id: '1', name: 'Ahorro', icon: 'ahorro' },
    { id: '2', name: 'Comida', icon: 'comida' },
    { id: '3', name: 'Casa', icon: 'casa' },
