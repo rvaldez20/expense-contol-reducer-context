f08f4c6ba737a8391860792bc6fe0e1fe85783f6
rvaldez20 <rvaldez20@gmail.com>
2024-04-09 18:32:38 -0600

    [ADD] use the custom hook useBudget for set the state.modal in close and add event on clic for close modal

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -19,7 +19,7 @@ export default function ExpenseModal() {
       </div>
 
       <Transition appear show={state.modal} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
+        <Dialog as="div" className="relative z-10" onClose={() => dispatch({type: 'close-modal'})}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
