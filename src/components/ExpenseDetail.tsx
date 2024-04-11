6ae8b9aa30a2b2018d3740a4d566013b496b4f9f
rvaldez20 <rvaldez20@gmail.com>
2024-04-11 14:57:36 -0600

    [ADD] create component for detail every expense

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,14 @@
+import { Expense } from "../types"
+
+type ExpenseDetailProps = {
+   expense: Expense
+}
+
+export default function ExpenseDetail({expense}: ExpenseDetailProps) {
+  return (
+    <div className="bg-white shadow-lg p-10 w-full borde-b border-gray-200">
+     
+
+    </div>
+  )
+}
