875ad5015ed18f6feeb8a576a15649422a11000d
rvaldez20 <rvaldez20@gmail.com>
2024-04-11 14:53:03 -0600

    [ADD] after to set the expend in state, clean form and close modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -71,7 +71,8 @@ export const budgetReducer = (
 
       return {
          ...state,
-         expenses:  [...state.expenses, expense ]
+         expenses:  [...state.expenses, expense ],
+         modal: false
       }
    }
 
