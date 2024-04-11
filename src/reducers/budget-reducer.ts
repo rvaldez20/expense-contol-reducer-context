40c6f8b346ded9edcdfc87971fd9d2db691ddd34
rvaldez20 <rvaldez20@gmail.com>
2024-04-11 14:18:58 -0600

    [ADD] add action add-expense, so create a function for chenge DrafExpense at Expense(whit id)

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,22 +1,36 @@
-
+import {v4 as uuidv4} from 'uuid'
+import { DraftExpense, Expense } from "../types"
 
 
 export type BudgetActions = 
    {type: 'add-budget', payload:{budget: number}} |
    {type: 'show-modal'} |
-   {type: 'close-modal'} 
+   {type: 'close-modal'} |
+   {type: 'add-expense', payload: {expense: DraftExpense}} 
 
 
 export type BudgetState = {
    budget: number,
    modal: boolean,
+   expenses: Expense[]
 }
 
 
 export const initialState:BudgetState = {
    budget: 0,
    modal: false,
+   expenses: [],
 }
+
+
+//* function for change drafExpense(no id) to Expense(whit id)
+const createExpense = (drafExpense: DraftExpense): Expense => {
+   return {
+      ...drafExpense,
+      id: uuidv4(),
+   }
+}
+
 
 
 export const budgetReducer = (
@@ -47,6 +61,17 @@ export const budgetReducer = (
       return {
          ...state,
          modal: false,
+      }
+   }
+
+
+   if(action.type === 'add-expense') {
+      //* ejecutamos la funcion que crea el Exense      
+      const expense = createExpense(action.payload.expense)
+
+      return {
+         ...state,
+         expenses:  [...state.expenses, expense ]
       }
    }
 
