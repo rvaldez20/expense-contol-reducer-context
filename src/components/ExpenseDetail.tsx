08ecca9ec0dd17637cfc5219b6d67852d7b1723c
rvaldez20 <rvaldez20@gmail.com>
2024-04-12 20:51:46 -0600

    [ADD] continue to show info of expense, show name, data and amount

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
    expense: Expense
@@ -6,8 +8,19 @@ type ExpenseDetailProps = {
 
 export default function ExpenseDetail({expense}: ExpenseDetailProps) {
   return (
-    <div className="bg-white shadow-lg p-10 w-full borde-b border-gray-200">
-     
+    <div className="bg-white shadow-lg p-10 w-full borde-b border-gray-200 flex gap-5 items-center">
+      <div>
+
+      </div>
+
+      <div>
+        <p>{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+      </div>
+
+      <AmountDisplay 
+        amount={expense.amount}
+      />
 
     </div>
   )
