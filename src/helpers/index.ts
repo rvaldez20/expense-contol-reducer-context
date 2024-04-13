8a20add0c0cd6b2427ad273aa3b256a44bcea504
rvaldez20 <rvaldez20@gmail.com>
2024-04-12 20:48:52 -0600

    [ADD] create helper function for format Date (martes, 30 de abril 2024)

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,16 @@
+//* Function format number to currency
 export function formatCurrency(amount: number) {
    return new Intl.NumberFormat('es-US', {style: 'currency', currency: 'USD'}).format(amount)
 }
+
+export function formatDate(dateStr: string): string {
+   const dateObj = new Date(dateStr)
+   const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+   }
+
+   return new Intl.DateTimeFormat('es-ES', options).format(dateObj)
+}  
