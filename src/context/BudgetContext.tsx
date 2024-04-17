fe7bb29afc1db12a92107fc49484cac709d1c8e3
rvaldez20 <rvaldez20@gmail.com>
2024-04-17 12:07:30 -0600

    [ADD] calculate the total expenses and remainingBudget and this available in the context

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,10 +1,12 @@
-import { useReducer, createContext, Dispatch, ReactNode } from 'react'
+import { useReducer, createContext, Dispatch, ReactNode, useMemo } from 'react'
 import { BudgetActions, BudgetState, budgetReducer, initialState } from '../reducers/budget-reducer'
 
 //! 4. creamos type para Props del BudgetContext
 type BudgetContextProps = {
    state: BudgetState
    dispatch: Dispatch<BudgetActions>
+   totalExpenses: number
+   remainingBudget: number
 }
 
 //! 5. creamos type para Props del BudgetProvider
@@ -21,11 +23,17 @@ export const BudgetProvider = ({children}:BudgetProviderProps) => {
    //! 2. Instanciamos el Reducer
    const [state, dispatch] = useReducer(budgetReducer, initialState)
 
+   //! state derivados para calculos de total gastado y resto del oresupuesto
+   const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0) ,[state.expenses])
+   const remainingBudget = state.budget - totalExpenses
+
    return (
       <BudgetContext.Provider
          value={{
             state,
             dispatch,
+            totalExpenses,
+            remainingBudget,
          }}
       >
          {children}
