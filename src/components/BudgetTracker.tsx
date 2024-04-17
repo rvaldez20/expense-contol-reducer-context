c235e5c685bb1a502783c15dc03de60d21135647
rvaldez20 <rvaldez20@gmail.com>
2024-04-17 12:11:34 -0600

    [ADD] display the total expense and remainingBudget

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,8 +1,13 @@
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 
 
 export default function BudgetTracker() {
+
+   const {state, totalExpenses, remainingBudget} = useBudget()
+   
+
    return (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
          <div className="flex justify-center">
@@ -19,19 +24,21 @@ export default function BudgetTracker() {
 
             <AmountDisplay 
                label="Presupuesto"
-               amount={300}
+               amount={state.budget}
             />
 
             <AmountDisplay 
                label="Disponible"
-               amount={200}
+               amount={remainingBudget}
             />
 
             <AmountDisplay 
                label="Gastado"
-               amount={100}
+               amount={totalExpenses}
             />
             
+            {Math.floor((totalExpenses / remainingBudget) * 100)}
+
          </div>
       </div>
    )
