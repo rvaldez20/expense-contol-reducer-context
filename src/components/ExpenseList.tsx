ad749159ea9c91f5b2189652ed46e57cfe740fa7
rvaldez20 <rvaldez20@gmail.com>
2024-04-17 18:41:01 -0600

    [ADD] filter categories using the state currentCategory and check if is empty for show label no expeses

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -7,15 +7,17 @@ import ExpenseDetail from "./ExpenseDetail"
 export default function ExpenseList() {
 
    const {state} = useBudget()
-   
-   const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
+
+   const filterExpenses = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory) : state.expenses
+
+   const isEmpty = useMemo(() => filterExpenses.length === 0, [filterExpenses])
 
    return (
       <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
-         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No expenses have been added yet</p> : (
+         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No expenses added yet</p> : (
             <>
                <p className="text-gray-600 text-2xl font-bold my-5">List Expenses</p>
-               {state.expenses.map(expense => (
+               {filterExpenses.map(expense => (
                   <ExpenseDetail 
                      key={expense.id}
                      expense={expense}
